b53fd86204dcbc0e9ec2596760f4d0c7557a90dd
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2019-01-10 00:03:41 -0800

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+// a game with 4 crystal and Random Result
+// Every crystal needs to have a random number between 1 - 12
+// When clicking any CRYSTAL, it should adding with the previous result
+// Until it equals the Random Result
+// If it is greater than the Random Result, we decrement a lost counter
+// If it is equal, then we increment a win counter
+// A new random number should be generate every single time we win or lost
+// to those 4 crystals
